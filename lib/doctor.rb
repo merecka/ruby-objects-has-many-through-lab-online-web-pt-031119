f28dd9a487e6004eb810f8bd6e0ea1774369e81a
Alex Merecka <merecka@gmail.com>
2019-04-13 20:38:56 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,5 +1,7 @@
 class Doctor
 
+  attr_accessor :name, :patient, :date
+
   @@all = []
 
   def self.all
@@ -10,5 +12,7 @@ class Doctor
     @name = name
     @@all << self
   end
+
+  def new_appointment(patient, date)
 
 end
